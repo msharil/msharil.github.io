581fdd53d04e3fff510ec5db923fe1fc052f614c
Mohd Sharil Sarifhudin <msharil1983@gmail.com>
2018-04-10 16:57:06 +0800

    Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,5 +8,61 @@ $("#frmMN").submit( function( event ) {
 	var mn = $("#txtFatLoss").val();
 	
 
-	alert('Button submitted! '+ name + ' ' + weight +'kg  MN' + mn);
+	if(name == '' || weight == '' || mn == '') {
+		alert('Sila lengkapkan maklumat');
+	} else {
+
+		// kira Protein
+		// P = MN x 40%
+		// gP = P / 4
+		var P = mn*0.4
+		var gP = parseFloat(P)/4
+
+		// kira Carbs
+		// C = MN x 30%
+		// gC = C / 4
+		var C = mn*0.3
+		var gC = parseFloat(C)/4
+
+		// kira Fat
+		// F = MN x 30%
+		// gF = F / 9
+		var F = mn*0.3
+		var gF = parseFloat(F)/9
+
+		//alert(' '+ name + ' ' + weight +'kg  MN' + mn);	
+		var str = '';
+
+		str += '<b>Nama: </b>' + name + ' <br>';
+		str += '<b>Berat: </b>' + weight + 'kg <br>';
+		str += '<b>Magic Number: </b>' + mn + ' <br>';
+		str += '<table class="table table-border table-striped">';
+
+		str += '<tr>';
+		str += '<th scope="row">Protein</th>';
+		str += '<td>'+ P +' kcal</td>';
+		str += '<td>'+ gP.toFixed(2) +' gram</td>';
+		str += '</tr>';
+
+		str += '<tr>';
+		str += '<th scope="row">Karbohidrat</th>';
+		str += '<td>'+ C +' kcal</td>';
+		str += '<td>'+ gC.toFixed(2) +' gram</td>';
+		str += '</tr>';
+
+		str += '<tr>';
+		str += '<th scope="row">Lemak Baik</th>';
+		str += '<td>'+ F +' kcal</td>';
+		str += '<td>'+ gF.toFixed(2) +' gram</td>';
+		str += '</tr>';
+
+		// str += '<p><b>Protein: </b>' + P +' kcal => ' + gP.toFixed(2) + 'g </p>';
+		// str += '<p><b>Karbo: </b>' + C +' kcal => ' + gC.toFixed(2) + 'g </p>';
+		// str += '<p><b>Lemak Baik: </b>' + F +' kcal => ' + gF.toFixed(2) + 'g </p>';
+
+		str += '</table>';
+		//alert('<p>Protein: ' + P + ' => ' + gP + 'g </p>');
+		$('#result').html(str);
+	}
+	
 });
